5c417cbbc216d272e3fbfee27b80a0ba8c6483e3
Gustavo Dalbem <gustavo.cdalbem@hotmail.com>
2025-01-17 19:32:40 -0300

    Switch case

--- Dia 5/index.js
+++ Dia 5/index.js
@@ -39,3 +39,49 @@ switch (ope) {
         console.log("pneus calibrados com sucesso")
         break
 }
+
+
+//CAÇA BUGS - CONDICIONAIS
+
+// BUGS NAO RESOLVIDO
+let iniciar = prompt("Deseja iniciar o programa? s/n")
+
+if (iniciar == "s") {
+    alert("Programa encerrado!")
+} else {
+    alert("Programa iniciado!")
+    let opcao = prompt("Escolha uma ação a ser feita -> 1 - Mensagem motivacional; 2 - Mensagem desmotivacional; 3 - nada")
+    switch (iniciar) {
+        case 1:
+            alert("Você NÃO VAI conseguir, desista!")
+        case 2:
+            alert("Continue tentando, você VAI conseguir!")
+        case 3:
+            alert("Opção inválida selecione um número de 1 a 3.")
+        default:
+            alert("...")
+    }
+}
+
+// BUGS RESOLVIDO 
+let iniciar = prompt("Deseja iniciar o programa? s/n")
+
+if (iniciar == "n") {
+    alert("Programa encerrado!")
+} else {
+    alert("Programa iniciado!")
+    let opcao = prompt("Escolha uma ação a ser feita -> 1 - Mensagem motivacional; 2 - Mensagem desmotivacional; 3 - nada")
+    switch (opcao) {
+        case "1":
+            alert("Continue tentando, você VAI conseguir!")
+            break
+        case "2":
+            alert("Você NÃO VAI conseguir, desista!")
+            break
+        case "3":
+            alert("...")
+            break
+        default:
+            alert("Opção inválida selecione um número de 1 a 3.")
+    }       
+}
