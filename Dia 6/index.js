ad5b999fbc605cd4c0db362c7760549e3c749515
Gustavo Dalbem <gustavo.cdalbem@hotmail.com>
2025-01-17 21:24:17 -0300

    For

---
+++ Dia 6/index.js
@@ -0,0 +1,32 @@
+// Exercicio 1: Contagem com Estrutura de Repetição
+
+let num1 = parseInt(prompt("Digite algum numero inteiro: ")) 
+
+for (contador = 0; contador <= num1; contador++) {
+    console.log(contador)
+}  
+
+// Exercicio 2: Contagem Progressiva de 5 em 5
+
+for(contagem  =0; contagem <= 10; contagem++) {
+    console.log(contagem * 5)
+}
+
+// Exercicio 3: Contagem Regressiva de 5 em 5 invertido
+
+for(contagem  = 50; contagem >= 0; contagem -= 5) {
+    console.log(contagem)
+}
+
+// Exercicio 4: Tabuada
+
+let numero = parseInt(prompt("Digite a um numero inteiro: "))
+
+for (let i = numero; i <= numero + 2; i++) {
+    console.log("tabuada do numero: " + i)
+    for (let j = 0; j <= 10; j++) {
+        console.log(i + " x " + j + " = " + (i * j))             
+    } 
+
+}
+
