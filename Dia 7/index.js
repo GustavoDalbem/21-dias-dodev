11c1fb564dca0bb9b6f11d2c349d9ceb28f2a9ac
Gustavo Dalbem <gustavo.cdalbem@hotmail.com>
2025-01-23 21:11:57 -0300

    While e DoWhile

--- Dia 7/index.js
+++ Dia 7/index.js
@@ -1,5 +1,5 @@
 //Exercio 1: DoDev School (While)
-/*
+
 let continuar = true;
 let somaNotas = 0;
 let totalAlunos = 0;
@@ -38,20 +38,55 @@ console.log(`Total de homens: ${totalHomens}`);
 console.log(`Mulheres com nota acima de 7: ${mulheresNotaAlta}`);
 console.log(`Maior nota entre os homens: ${maiorNotaHomens}`);
 
-*/
 
 // Exercicio 2: Caixa eletronico DoDev (DOWHILE)
 
+let saldo = 1000; // Inicializamos o saldo com um valor inicial
+let maiorValorInserido = 0;
+let somaValoresInseridos = 0;
+let totalTransacoes = 0;
+let continuar = false;
+
 do {
-    const nome = prompt("Digite o seu nome: ")
-    const cpf = prompt("Digite seu CPF: ")
-    const valor = Number(prompt("Digite o valor da transição: "))
-    const opcao = prompt("Qual operacao voce deseja realizar? (S / D) ")
+    const nome = prompt("Digite seu nome:");
+    const cpf = prompt("Digite seu CPF:");
+    const valor = Number(prompt("Digite o valor da transação: "));
+    const operacao = prompt("Qual operacao deseja fazer? S / D");
 
-    if(valor < 0) {
-        console.log("Valor invalido, A transação nao foi realiazda")
-    } else if (opcao === "s" && valor > saldo) {
-        
-
+    if (valor < 0) {
+        console.log("Valor inválido. A transação não foi realizada.");
+    } else if (operacao == "S" && valor > saldo) {
+        console.log("Saldo insuficiente. A transação não foi realizada");
+    } else if (operacao == "S") {
+        console.log(`Olá, ${nome} (${cpf}), seu saldo atual é R$${saldo}.`);
+        saldo -= valor;
+        totalTransacoes++;
+        somaValoresInseridos += valor;
+        if (valor > maiorValorInserido) {
+            maiorValorInserido = valor;
+        }
+    } else {
+        console.log(`Olá, ${nome} (${cpf}), seu saldo atual é R$${saldo}.`);
+        saldo += valor;
+        totalTransacoes++;
+        somaValoresInseridos += valor;
+        if (valor > maiorValorInserido) {
+            maiorValorInserido = valor;
+        }
+        console.log(`Transação realizada com sucesso. Seu saldo atual é R$${saldo}.`);
     }
-}
+
+    const opcao = prompt("Deseja continuar? (1 para continuar, 2 para parar):");
+    if (opcao === "1") {
+        continuar = true;
+    } else if (opcao === "2") {
+        continuar = false;
+    } else {
+        console.log("Opção inválida. Programa encerrado.");
+        continuar = false;
+    }
+} while (continuar);
+
+console.log(`Saldo final: R$${saldo}`);
+console.log(`Maior valor inserido: R$${maiorValorInserido}`);
+console.log(`Média dos valores inseridos: R$${somaValoresInseridos / totalTransacoes}`);
